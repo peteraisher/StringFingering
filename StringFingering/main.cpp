98af8f6289c49075d5faddd75f37217b21ea8435
Peter Aisher <peter.aisher@me.com>
2021-09-28 18:11:56 +0200

    Initial Commit

---
+++ StringFingering/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StringFingering
+//
+//  Created by Peter Aisher on 28.09.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
