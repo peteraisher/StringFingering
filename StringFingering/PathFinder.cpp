99d0088d0db8e77d076c672ac6f0f1100c664e49
Peter Aisher <peter.aisher@me.com>
2022-05-21 21:52:32 +0200

    refactor PathFinder

--- StringFingering/PathFinder.cpp
+++ StringFingering/PathFinder.cpp
@@ -50,60 +50,85 @@ void PathFinder::advanceHelperVariables() {
   ++index;
 }
 
+void PathFinder::resetBests() {
+  best_string = -1;
+  best_finger = -1;
+  best_score = std::numeric_limits<score_t>::max();
+}
+
+void PathFinder::evaluatePreviousFinger(int fPrev, int sPrev) {
+  string_t stringDiff = string - sPrev;
+  score_t fingerScore = stringScore
+  + delegate->fingerChangePenalty(posPrev, pos, fPrev, finger, stringDiff)
+  + scores.previous(sPrev, fPrev);
+  if (fingerScore < best_score) {
+    best_score = fingerScore;
+    best_string = sPrev;
+    best_finger = fPrev;
+  }
+}
+
+void PathFinder::evaluatePreviousString(int sPrev) {
+  posPrev = positions.previous(sPrev);
+  if (posPrev < 0) {
+    return;   // previous pitch was not playable on previous string
+  }
+  stringScore = delegate->stringCrossPenalty(string - sPrev) + rawScore;
+  if (stringScore >= best_score) {
+    return;   // all possible scores are >= stringScore so also >= best_score
+  }
+
+  for (int fPrev = 0; fPrev < kFingerCount; ++fPrev) {
+    // compare position difference to finger difference
+    evaluatePreviousFinger(fPrev, sPrev);
+  }
+}
+
+void PathFinder::setNeverScoreIfNothingFound() {
+  if (best_string == -1
+      || best_finger == -1
+      || best_score == std::numeric_limits<score_t>::max()) {
+    best_score = delegate->never();
+  }
+}
+
+void PathFinder::storeBestScoreInMap() {
+  map_t prev_pointer = (best_finger << kStringBits) | best_string;
+  scores.current(string, finger) = best_score;
+  map.value(index, string, finger) = prev_pointer;
+}
+
+void PathFinder::findAndStoreBestTransitionToCurrentStringAndFinger() {
+  rawScore = delegate->rawPositionScore(pos, string, finger);
+  resetBests();
+  for (int sPrev = 0; sPrev < stringCount; ++sPrev) {
+    evaluatePreviousString(sPrev); // end for fPrev
+  } // end for sPrev
+  setNeverScoreIfNothingFound();
+  storeBestScoreInMap();
+}
+
+void PathFinder::evaluateScoresForString() {
+  if (pos < 0) {
+    markStringAsImpossibleForCurrentPitch(string);
+    return;
+  }
+  positions.current(string) = pos;
+
+  for (finger = 0; finger < kFingerCount; ++finger) {
+    findAndStoreBestTransitionToCurrentStringAndFinger();
+  }
+}
+
 void PathFinder::appendPitch(uint8_t pitch) {
   advanceHelperVariables();
-
-  for (int s = 0; s < stringCount; ++s) {
-    position_t pos = pitch - delegate->openStringPitch(s);
-    if (pos < 0) {
-      markStringAsImpossibleForCurrentPitch(s);
-      continue;
-    }
-    positions.current(s) = pos;
-
-    for (int f = 0; f < kFingerCount; ++f) {
-      score_t rawScore = delegate->rawPositionScore(pos, s, f);
-      string_t best_string = -1;
-      finger_t best_finger = -1;
-      score_t best_score = std::numeric_limits<score_t>::max();
-      for (int sPrev = 0; sPrev < stringCount; ++sPrev) {
-        position_t posPrev = positions.previous(sPrev);
-        if (posPrev < 0) {
-          continue;
-        }
-        score_t stringScore = delegate->stringCrossPenalty(s - sPrev) + rawScore;
-        if (stringScore > best_score) {
-          continue;
-        }
-        for (int fPrev = 0; fPrev < kFingerCount; ++fPrev) {
-          // compare position difference to finger difference
-          string_t stringDiff = s - sPrev;
-          score_t fingerScore = stringScore
-            + delegate->fingerChangePenalty(posPrev, pos, fPrev, f, stringDiff)
-            + scores.previous(sPrev, fPrev);
-          if (fingerScore < best_score) {
-            best_score = fingerScore;
-            best_string = sPrev;
-            best_finger = fPrev;
-          }
-        } // end for fPrev
-      } // end for sPrev
-      if (best_string == -1
-          || best_finger == -1
-          || best_score == std::numeric_limits<score_t>::max()) {
-        best_score = delegate->never();
-      }
-      map_t prev_pointer = (best_finger << kStringBits) | best_string;
-      scores.current(s, f) = best_score;
-      map.value(index, s, f) = prev_pointer;
-    } // end for f
+  for (string = 0; string < stringCount; ++string) {
+    pos = pitch - delegate->openStringPitch(string);
+    evaluateScoresForString();
   } // end for s
 }
 
-FingeringSequence PathFinder::readOutput() {
-  string_t best_string = -1;
-  finger_t best_finger = -1;
-  score_t best_score = std::numeric_limits<score_t>::max();
+void PathFinder::findBestFingerAndStringForCurrentPitch() {
   for (int s = 0; s < stringCount; ++s) {
     for (int f = 0; f < kFingerCount; ++f) {
       if (scores.current(s, f) < best_score) {
@@ -113,16 +138,29 @@ FingeringSequence PathFinder::readOutput() {
       }
     }
   }
+}
+
+void PathFinder::readPreviousBestFingerAndStringFromMap(size_t i) {
+  map_t m = map.value(i, best_string, best_finger);
+  best_string = m & kStringMask;
+  best_finger = m >> kStringBits;
+}
+
+void PathFinder::setFingeringFromBestAtIndex(size_t i,
+                                             FingeringSequence &result) {
+  result.setFinger(i, best_finger);
+  result.setString(i, best_string);
+}
+
+FingeringSequence PathFinder::readOutput() {
+  resetBests();
+  findBestFingerAndStringForCurrentPitch();
   FingeringSequence result = FingeringSequence(index + 1);
   for (size_t i = index; i > 0; --i) {
-    result.setFinger(i, best_finger);
-    result.setString(i, best_string);
-    map_t m = map.value(i, best_string, best_finger);
-    best_string = m & kStringMask;
-    best_finger = m >> kStringBits;
+    setFingeringFromBestAtIndex(i, result);
+    readPreviousBestFingerAndStringFromMap(i);
   }
-  result.setFinger(0, best_finger);
-  result.setString(0, best_string);
+  setFingeringFromBestAtIndex(0, result);
   return result;
 }
 
--- StringFingering/PathFinder.hpp
+++ StringFingering/PathFinder.hpp
@@ -26,11 +26,31 @@ class PathFinder {
   size_t stringCount = 0;
   size_t kStringBits = 0;
   size_t kStringMask = 1;
+  score_t rawScore = 0;
+  score_t stringScore = 0;
+  score_t best_score = 0;
+  position_t pos = 0;
+  position_t posPrev = 0;
+  string_t best_string = 0;
+  finger_t best_finger = 0;
+  int string = 0;
+  int finger = 0;
+
   enum {
     kFingerCount = 5
   };
   void markStringAsImpossibleForCurrentPitch(int s);
   void advanceHelperVariables();
+  void resetBests();
+  void evaluatePreviousFinger(int fPrev, int sPrev);
+  void evaluatePreviousString(int sPrev);
+  void setNeverScoreIfNothingFound();
+  void storeBestScoreInMap();
+  void findAndStoreBestTransitionToCurrentStringAndFinger();
+  void evaluateScoresForString();
+  void findBestFingerAndStringForCurrentPitch();
+  void readPreviousBestFingerAndStringFromMap(size_t i);
+  void setFingeringFromBestAtIndex(size_t i, FingeringSequence &result);
 public:
   PathFinder(size_t noteCount, OptimizerDelegate* delegate);
   void setFirstPitch(uint8_t pitch);
